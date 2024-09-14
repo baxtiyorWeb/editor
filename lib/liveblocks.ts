9266785047269bf88179f343c755b0e9986d9654
baxtiyorWeb <baxtiyorqurbonnazarov33@gmail.com>
2024-09-14 10:11:09 +0500

    ok

--- lib/liveblocks.ts
+++ lib/liveblocks.ts
@@ -1,5 +1,7 @@
 import { Liveblocks } from "@liveblocks/node";
+import env from 'dotenv'
 
+env.config()
 export const liveblocks = new Liveblocks({
   secret: process.env.LIVEBLOCKS_SECRET_KEY as string,
 });
